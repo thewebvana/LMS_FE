a729f44e853fe6b6ab12a4e7a27c3740489a8de5
Sourabh garewal <28saurbh@gmail.com>
2025-03-05 23:52:49 +0530

    Update login-form.jsx

--- src/components/ui/login-form.jsx
+++ src/components/ui/login-form.jsx
@@ -1,66 +1,62 @@
-"use client"
+"use client";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Link } from "react-router-dom"
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Link } from "react-router-dom";
 
-export function LoginForm({
-  className,
-  ...props
-}) {
-  return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="abc@example.com" />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
-                    Forgot your password?
-                  </a>
-                </div>
-                <Input id="password" type="password" />
-              </div>
-              <Link to="/dashboard">
-                <Button className="w-full">
-                  Login
-                </Button>
-              </Link>
-            </div>
-            <div className="mt-4 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link to="/signup">
-                <Button variant='link' size='none'>
-                  Register
-                </Button>
-              </Link>
-            </div>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  );
+export function LoginForm({ className, ...props }) {
+	return (
+		<div className={cn("flex flex-col gap-6", className)} {...props}>
+			<Card>
+				<CardHeader>
+					<CardTitle className="text-2xl">Login</CardTitle>
+					<CardDescription>
+						Enter your email below to login to your account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form>
+						<div className="flex flex-col gap-6">
+							<div className="grid gap-4">
+								<Label htmlFor="email">Email</Label>
+								<Input id="email" type="email" placeholder="abc@example.com" />
+							</div>
+							<div className="grid gap-4">
+								<div className="flex items-center">
+									<Label htmlFor="password">Password</Label>
+								</div>
+								<Input id="password" type="password" />
+							</div>
+							<a
+								href="#"
+								className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+							>
+								Forgot your password?
+							</a>
+							<Link to="/dashboard">
+								<Button className="w-full">Login</Button>
+							</Link>
+						</div>
+						<div className="mt-4 text-center text-sm">
+							Don&apos;t have an account?{" "}
+							<Link to="/signup">
+								<Button variant="link" size="none">
+									Register
+								</Button>
+							</Link>
+						</div>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
+	);
 }
